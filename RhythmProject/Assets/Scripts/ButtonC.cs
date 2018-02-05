bcb58c2f93e4551bcd6dd5f8cc13b89d39ad7f42
ashley <ah3278@nyu.edu>
2018-02-04 23:38:55 -0500

    spawn notes every 50 frames

---
+++ RhythmProject/Assets/Scripts/ButtonC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonC : MonoBehaviour {
+
+	private GameObject buttonC;
+
+	// Use this for initialization
+	void Start () {
+		if (buttonC == null) {
+			buttonC = GameObject.FindWithTag ("C");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			buttonC.GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+	}
+
+	void onCollisionEnter(){
+		
+	}
+}
---
+++ RhythmProject/Assets/Scripts/SpawnNote.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNote : MonoBehaviour {
+	
+	public GameObject note;
+	private float timer;
+	private bool appeared;
+	private List<GameObject> arrayOfNotes = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		timer = 50.0f;
+		appeared = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer--;
+		//Debug.Log (timer);
+		if (timer <= 0f && appeared == false) {
+			GameObject newNote = Instantiate (note, new Vector3(0,5,0), transform.rotation);
+			arrayOfNotes.Add (newNote);
+			appeared = true;
+			timer = 50.0f;
+		} else {
+			appeared = false;
+		}
+
+		if (arrayOfNotes.Count > 0) {
+			foreach(GameObject item in arrayOfNotes)
+			{
+				item.transform.position -= transform.up * Time.deltaTime * 1;
+			}
+		}
+	}
+}
