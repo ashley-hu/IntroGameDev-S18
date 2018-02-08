d172e5a452bdb47810c03857785dea24b27560d5
ashley <ah3278@nyu.edu>
2018-02-08 17:45:32 -0500

    loaded resources

--- RhythmProject/Assets/Scripts/GameManager.cs
+++ RhythmProject/Assets/Scripts/GameManager.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public AudioClip audioSource;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +30,20 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void LoadSongOne(){
+		if (SceneManager.GetActiveScene ().buildIndex == 0) {
+
+			SceneManager.LoadScene (""); 
+			audioSource = Resources.Load<AudioClip>("sample");
+			//load text file 
+		}
+	}
+
+	public void LoadSongTwo(){
+		if (SceneManager.GetActiveScene ().buildIndex == 0) {
+			
+		}
 	}
 }
--- RhythmProject/Assets/Scripts/SpawnNote.cs
+++ RhythmProject/Assets/Scripts/SpawnNote.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SpawnNote : MonoBehaviour {
 	
@@ -11,6 +12,20 @@ public class SpawnNote : MonoBehaviour {
 	private float[] arrayOfColumn = new float[4];
 	private int x;
 
+	private AudioClip songOne;
+	private TextAsset songData;
+	private string textSongData;
+	public AudioSource songSource;
+	string[] lines;
+
+	void Awake() {
+		songSource = GetComponent<AudioSource>();
+		songData = Resources.Load ("sampletext") as TextAsset;
+		songOne = Resources.Load<AudioClip>("sample");
+		//Debug.Log (songOne);
+		//Debug.Log (songData);
+	}
+
 	// Use this for initialization
 	void Start () {
 		timer = 60.0f;
@@ -20,31 +35,47 @@ public class SpawnNote : MonoBehaviour {
 		arrayOfColumn [1] = -0.5f;
 		arrayOfColumn [2] = 0.5f;
 		arrayOfColumn [3] = 1.5f;
+		textSongData = songData.text;
+		ParseSongFile (textSongData);
+
+		songSource.clip = songOne;
+		songSource.Play ();
+		Debug.Log (songSource);
+	}
+
+	void ParseSongFile(string x){
+		lines = x.Split('\n');
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer--;
-		if (timer <= 0f && appeared == false) {
-			x = Random.Range (0, 4);
-			GameObject newNote = Instantiate (note, new Vector3(arrayOfColumn[x],5,0), transform.rotation);
-			arrayOfNotes.Add (newNote);
-			appeared = true;
-			timer = 50.0f;
-		} else {
-			appeared = false;
+//		timer--;
+//		if (timer <= 0f && appeared == false) {
+//			x = Random.Range (0, 4);
+//			GameObject newNote = Instantiate (note, new Vector3(arrayOfColumn[x],5,0), transform.rotation);
+//			arrayOfNotes.Add (newNote);
+//			appeared = true;
+//			timer = 50.0f;
+//		} else {
+//			appeared = false;
+//		}
+//
+//		if (arrayOfNotes.Count > 0) {
+//			for(int i=arrayOfNotes.Count-1; i>=0; i--){
+//				//Debug.Log (arrayOfNotes.Count);
+//				//Debug.Log (arrayOfNotes [i]);
+//				arrayOfNotes[i].transform.position -= arrayOfNotes[i].transform.up * Time.deltaTime * 5;
+//				if (arrayOfNotes[i].transform.position.y < -4.5f) {
+//					Destroy (arrayOfNotes[i]);
+//					arrayOfNotes.RemoveAt (i);
+//					//Debug.Log ("Miss");
+//				}
+//			}
+//		}
+
+		//if(songSource.timeSamples)
+		for(int i=0;i < lines.Length; i++){
 		}
 
-		if (arrayOfNotes.Count > 0) {
-			for(int i=arrayOfNotes.Count-1; i>=0; i--){
-				//Debug.Log (arrayOfNotes.Count);
-				//Debug.Log (arrayOfNotes [i]);
-				arrayOfNotes[i].transform.position -= arrayOfNotes[i].transform.up * Time.deltaTime * 5;
-				if (arrayOfNotes[i].transform.position.y < -4.5f) {
-					Destroy (arrayOfNotes[i]);
-					arrayOfNotes.RemoveAt (i);
-				}
-			}
-		}
 	}
 }
