fbc387409f40659f70938cf75eec85263b44ed0e
ashley <ah3278@nyu.edu>
2018-03-04 19:25:05 -0500

    fixed boss image and created animation

--- RhythmProject/Assets/Scripts/BossMovement.cs
+++ RhythmProject/Assets/Scripts/BossMovement.cs
@@ -14,13 +14,16 @@ public class BossMovement : MonoBehaviour {
 	RectTransform bossImage;
 	float reverse;
 	Image img;
+	public Sprite rightImage;
+	public Sprite leftImage;
 
 	// Use this for initialization
 	void Start () {
 		img = gameObject.GetComponent<Image> ();
 		bossImage = gameObject.GetComponent<RectTransform> ();
-		bossImage.localPosition = new Vector3 (0, 100, 0); //get boss's local position on canvas
+		bossImage.localPosition = new Vector3 (0, 220, 0); //get boss's local position on canvas
 		reverse = 1;
+		img.sprite = rightImage;
 	}
 
 	// Update is called once per frame
@@ -28,10 +31,12 @@ public class BossMovement : MonoBehaviour {
 		//if the boss is still alive, move the boss to the right and reverse if position is greater than 30, vice versa
 		if (GameManager.bossCurrHealth > 0) {
 			bossImage.position += Vector3.right * Time.deltaTime * 5 * reverse;
-			if (bossImage.localPosition.x > 30) { //move right then reverse
+			if (bossImage.localPosition.x > 50) { //move right then reverse
 				reverse = -1;
-			} else if (bossImage.localPosition.x < -30) { //move left then reverse 
+				img.sprite = leftImage;
+			} else if (bossImage.localPosition.x < -50) { //move left then reverse 
 				reverse = 1;
+				img.sprite = rightImage;
 			}
 		} else {
 			//fade the boss out if the boss health reaches 0
