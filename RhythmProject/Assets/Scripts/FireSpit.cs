2df767d471560ad17d44dbff424faa6f274579d5
ashley <ah3278@nyu.edu>
2018-03-21 17:28:21 -0400

    Spit Fire Animation Attempt

---
+++ RhythmProject/Assets/Scripts/FireSpit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpit : MonoBehaviour {
+
+	public Animator anim;
+	public bool setFire;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		setFire = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("spitFire", setFire);
+	}
+}
